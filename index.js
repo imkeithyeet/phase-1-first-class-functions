20f0be78a2f77e6793a25256bf2f3880e3331161
Keith Ma <keithchezzy@gmail.com>
2022-10-31 23:08:55 -0700

    commit tree 2f49d8939decae90752daac471fb1028444af7e0

---
+++ index.js
@@ -0,0 +1,9 @@
+const receivesAFunction=(theFunction)=>{
+    return theFunction();
+}
+const returnsANamedFunction=()=>{
+    return returnsANamedFunction;
+}
+const returnsAnAnonymousFunction=()=>{
+    return function (){};
+}
